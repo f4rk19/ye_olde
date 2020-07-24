897cbcee809437342def9d47764660b828f1a20a
f4rk19 <67687475+f4rk19@users.noreply.github.com>
2020-07-25 00:19:03 +0900

    view
    not yet

--- app/controllers/hashtags_controller.rb
+++ app/controllers/hashtags_controller.rb
@@ -11,7 +11,7 @@ class HashtagsController < ApplicationController
 
   def show
     @hashtag = Hashtag.find_by!(hashname: params[:hashname])
-    @photos = @hashtag.photos.page(params[:page]).per(10)
+    @photos = @hashtag.photos.page(params[:page]).per(12)
   end
 
   def search
