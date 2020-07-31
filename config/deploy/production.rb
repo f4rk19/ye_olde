1915671dd20416acb1fa325da673dc8b4453f037
f4rk19 <67687475+f4rk19@users.noreply.github.com>
2020-07-31 09:59:43 +0900

    deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -59,4 +59,4 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
-server '54.150.130.231', user: 'ec2-user', roles: %w{app db web }
+server '13.231.96.153', user: 'ec2-user', roles: %w{app db web }
