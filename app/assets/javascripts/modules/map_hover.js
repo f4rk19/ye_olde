7e4081350a9b603a73e4e103dd6bf57f04d6d8d0
f4rk19 <67687475+f4rk19@users.noreply.github.com>
2020-08-04 00:28:39 +0900

    solo text-align

---
+++ app/assets/javascripts/modules/map_hover.js
@@ -0,0 +1,8 @@
+// $(function(){
+//   $("#map-japan").on("mouseover", function(){
+//     $(".japan-tag").addClass("is-show")
+//   });
+//   $("#map-japan").on("mouseout",function(){
+//     $(".japan-tag").removeClass("is-show")
+//   });
+// });
